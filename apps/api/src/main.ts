1df05089ab79c44b23f5e87b8b34b60ccbf1154f
Thach Duong <thach.hongduong@coxautoinc.com>
2022-07-14 11:48:07 -0400

    add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -14,6 +15,16 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  const config = new DocumentBuilder()
+    .setTitle('Security Scan Result CRUD')
+    .setDescription('The security scan result API description')
+    .setVersion('1.0')
+    // .addTag('scan result')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
--- apps/api/src/scan-result/scan-result.dto.ts
+++ apps/api/src/scan-result/scan-result.dto.ts
@@ -1,27 +1,34 @@
 import { IsDate, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateScanResultDTO {
 
   @IsString()
+  @ApiProperty()
   status: string;
 
   @IsString()
+  @ApiProperty()
   repositoryName: string;
   
   @IsString()
   @IsOptional()
+  @ApiProperty({ required: false })
   findings: string;
 
   @IsDate()
   @IsOptional()
+  @ApiProperty({ required: false })
   queuedAt: Date;
 
   @IsDate()
   @IsOptional()
+  @ApiProperty({ required: false })
   finishedAt: Date;
 
   @IsDate()
   @IsOptional()
+  @ApiProperty({ required: false })
   scanningAt: Date;
 
 }
@@ -30,26 +37,32 @@ export class CreateScanResultDTO {
 export class UpdateScanResultDTO {
 
   @IsString()
+  @ApiProperty()
   status: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ required: false })
   repositoryName: string;
   
   @IsString()
   @IsOptional()
+  @ApiProperty({ required: false })
   findings: string;
 
   @IsDate()
   @IsOptional()
+  @ApiProperty({ required: false })
   queuedAt: Date;
 
   @IsDate()
   @IsOptional()
+  @ApiProperty({ required: false })
   finishedAt: Date;
 
   @IsDate()
   @IsOptional()
+  @ApiProperty({ required: false })
   scanningAt: Date;
 
 }
